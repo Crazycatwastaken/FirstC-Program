036f7e5b9fced293babbead39f7a68cb10d5364a
Crazycatwastaken <maxrasmussen4@gmail.com>
2024-07-14 16:34:53 +1000

    Svelte like $ and {} much better then , object imo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,16 +9,10 @@ namespace ConsoleApp1
             string name = "Max";
             int age = 18;
 
-            Console.WriteLine("Name: " + name);
-            Console.WriteLine("Age: " + age);
-
-            Console.WriteLine();
-            Console.WriteLine("Name: " + name + "\nAge: " + age);
+           
             Console.WriteLine("Your name is " + name + " and your age is " + age);
-            // , adds an object to replace {0/1}
-            Console.WriteLine("Your name is {0}, and your age is {1}", name, age);
-            Console.WriteLine("Name: {0}\nAge: {1}", name, age);
-            
+            Console.WriteLine($"Your name is {name} and your {age} is age");
+          
             Console.ReadLine();
         }
         
