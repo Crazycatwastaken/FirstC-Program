7bd44a6ac3f507cd861934e0cf6ffe2dcedcf1de
Crazycatwastaken <maxrasmussen4@gmail.com>
2024-07-15 14:28:55 +1000

    Password checker.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,27 +5,41 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.Write("What is your message: ");
-            string message = Console.ReadLine();
+        {
+            // Sets empty password
+            string password = null;
 
-
-
-            for (int i = 0; i < message.Length; i++)
+            // Runs this while password is empty.
+            while (string.IsNullOrEmpty(password))
             {
-                Console.Write(message[i]);
+                Console.Write("Enter your password: ");
+                password = Console.ReadLine();
+                // Checks if this password is empty and promts the user to enter a password.
+                if (string.IsNullOrEmpty(password))
+                {
+                    Console.WriteLine("Please enter a password ");
+                }
+            }
+        
+            // Checks if this password is not empty
+            if (!string.IsNullOrEmpty(password))
+            {
+                // Asks to re-enter password
+                Console.Write("Please re-enter your password: ");
+            
+                // Checks if password is equal to the readline. (The password the user entered again)
+                if (password.Equals(Console.ReadLine()))
+                {
+                    Console.WriteLine("Your passwords match");
+                }
+                else
+                {
+                    Console.WriteLine("Your passwords do not match");
+                }
+                Console.ReadLine();
             }
 
-            Console.WriteLine();
-            
-            for (int i = message.Length - 1; i >= 0; i--)
-            {
-                Console.Write(message[i]);
-            }
 
-            
-
-            Console.ReadLine();
         }
         
     }
