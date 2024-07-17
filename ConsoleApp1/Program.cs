d34306c9c15d6b24b4d234b84dc37e6026c34909
Crazycatwastaken <maxrasmussen4@gmail.com>
2024-07-17 12:01:50 +1000

    Functions and public functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,49 +3,29 @@
 
 namespace ConsoleApp1
 {
+
+    class Test
+    { 
+        // Function outside program
+        public static void Something()
+        {
+            Console.WriteLine("Something");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var even = new List<int>();
-            var odd = new List<int>();
-            int numbers;
-
-            Console.WriteLine("How many numbers: ");
-            numbers = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < numbers; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    even.Add(i);
-                }
-                else
-                {
-                    odd.Add(i);
-                }
-            }
-
-            Console.WriteLine("Even Numbers:");
-
-            foreach (var item in even)
-            {
-                Console.Write($"{item} ");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Odd Numbers:");
-            
-            foreach (var item in odd)
-            {
-                Console.Write($"{item} ");
-            }
-            
+            WelcomeMessage();
+            Test.Something();
             Console.ReadLine();
             
-
-
         }
-        
+
+       // Function within program
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome Max");
+        }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9abffc6824298c68bf22906475eb175e11bb1a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+129ebee71f13587e89885b7d9becedb10d90b888")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
